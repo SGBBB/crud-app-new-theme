f10cb689d94271b962ea2753376db88cbcdcd46c
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2020-06-20 22:51:52 +0800

    Updated with new UI

---
+++ js/loader.js
@@ -0,0 +1,16 @@
+// function showLoader(){
+
+//     $("#loader").addClass('loader');
+// }
+
+// function hideLoader(){
+//     $("#loader").removeClass('loader');
+// }
+
+function showLoader() {
+	$("#global-loader-div").show();
+}
+
+function hideLoader() {
+	$("#global-loader-div").hide();
+}
